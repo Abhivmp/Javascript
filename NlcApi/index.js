260277b8ef842f91e93a64f74c3acde85f121903
Abhivmp <99750438+Abhivmp@users.noreply.github.com>
2022-02-23 20:58:47 +0530

    Add files via upload

---
+++ NlcApi/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./classComponent/NLCComponent";
+ReactDOM.render(<App></App>, document.getElementById("root"))
+
+
+
---
+++ NlcApi/src/classComponent/NLCComponent.js
@@ -0,0 +1,57 @@
+   
+import { Component } from "react";
+import Axios from "axios";
+
+export default class NLCComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            NLC_Data: {}
+        }
+    }
+
+    render() {
+        return (
+            <div id="holow">
+                <h3 >NLC India Power Limited</h3>
+                <br />
+                <div className="center" style={{ width: '450px', margin: '20px', display: 'inline-block' }}>
+                    <div className="card-header">
+                        {this.state.NLC_Data.DISPID} {this.state.NLC_Data.SC_SUBSEC}
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">{this.state.NLC_Data.company}</h5>
+                        <div class="alert alert-primary" role="alert">
+                            Before Changed Price : {this.state.NLC_Data.priceprevclose}
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                           After Price Change :  {this.state.NLC_Data.pricechange}
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                            % of Price Change : {this.state.NLC_Data.pricepercentchange}
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+
+                            Current Price : {this.state.NLC_Data.pricecurrent}
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            Last Updated  : {this.state.NLC_Data.lastupd}
+                        </div>
+                    </div>
+                    <div className="card-footer text-muted">
+                        Year Details: {this.state.NLC_Data.YR}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        Axios.get("https://priceapi.moneycontrol.com/pricefeed/nse/equitycash/NLC").then((response) => {
+
+            this.setState({ NLC_Data: response.data.data })
+            console.log(this.state.NLC_Data)
+        });
+    }
+
+}
